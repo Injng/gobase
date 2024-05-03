9d62d9d6623de1062c5aba02ea0499351be698c0
Injng <jlinjiang@gmail.com>
2024-05-03 08:52:18 -0700

    move piece validation to backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,52 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::Mutex;
+
+#[derive(Clone)]
+enum Intersection {
+    Empty,
+    Black(Group),
+    White(Group),
+}
+
+struct Board {
+    pieces: Mutex<Vec<Vec<Intersection>>>,
+}
+
+#[derive(Clone)]
+struct Group {
+    intersections: Vec<(usize, usize)>,
+    liberties: Vec<(usize, usize)>,
+}
+
+// check if a given move is valid
+#[tauri::command]
+fn validate(x: usize, y: usize, color: usize, board: tauri::State<Board>) -> bool {
+    let mut board = board.pieces.lock().unwrap();
+    let intersection = &board[x][y];
+    let is_valid: bool;
+    match intersection {
+        Intersection::Empty => is_valid = true,
+        _ => is_valid = false,
+    }
+
+    // handle whether or not move is valid
+    if is_valid {
+        if color == 1 { 
+            board[x][y] = Intersection::Black(Group { intersections: vec![(x, y)], liberties: vec![] }); 
+        } else { 
+            board[x][y] = Intersection::White(Group { intersections: vec![(x, y)], liberties: vec![] }); 
+        }
+        return true;
+    }
+    return false;
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(Board { pieces: Mutex::new(vec![vec![Intersection::Empty; 19]; 19])})
+        .invoke_handler(tauri::generate_handler![validate])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,93 +1,91 @@
 <script lang="ts">
-import { onMount } from 'svelte'
+    import { onMount } from 'svelte'
+    import { invoke } from '@tauri-apps/api/tauri'
 
-let board: HTMLCanvasElement
-let hover: HTMLCanvasElement
-let pieces: HTMLCanvasElement
-const ROWS: number = 10
-const COLS: number = 10
-const GAP: number = 60
-let width: number = (COLS + 2) * GAP
-let height: number = (ROWS + 2) * GAP
-let boardX: number = 0
-let boardY: number = 0
-let ctxBoard: CanvasRenderingContext2D
-let ctxHover: CanvasRenderingContext2D
-let ctxPieces: CanvasRenderingContext2D
-let pieceColor: string = 'black'
+    let board: HTMLCanvasElement
+    let hover: HTMLCanvasElement
+    let pieces: HTMLCanvasElement
+    const ROWS: number = 10
+    const COLS: number = 10
+    const GAP: number = 60
+    let width: number = (COLS + 2) * GAP
+    let height: number = (ROWS + 2) * GAP
+    let boardX: number = 0
+    let boardY: number = 0
+    let ctxBoard: CanvasRenderingContext2D
+    let ctxHover: CanvasRenderingContext2D
+    let ctxPieces: CanvasRenderingContext2D
+    let pieceColor: string = 'black'
 
-// 2d array for the board, black = 1, white = 2
-let boardArray: number[][] = Array.from({ length: ROWS }, () => Array.from({ length: COLS }, () => 0))
+    onMount(() => {
+        ctxBoard = board.getContext('2d')
+        ctxHover = hover.getContext('2d')
+        ctxHover.globalAlpha = 0.3
+        ctxPieces = pieces.getContext('2d')
 
-onMount(() => {
-    ctxBoard = board.getContext('2d')
-    ctxHover = hover.getContext('2d')
-    ctxHover.globalAlpha = 0.3
-    ctxPieces = pieces.getContext('2d')
+        // draw board
+        ctxBoard.fillStyle = '#dcae6b'
+        ctxBoard.fillRect(0, 0, width, height)
+        ctxBoard.beginPath()
 
-    // draw board
-    ctxBoard.fillStyle = '#dcae6b'
-    ctxBoard.fillRect(0, 0, width, height)
-    ctxBoard.beginPath()
+        // draw board lines
+        let x: number = GAP
+        let y: number = GAP 
+        while (x < width || y < height) {
+            ctxBoard.moveTo(x, GAP)
+            ctxBoard.lineTo(x, GAP + GAP * ROWS)
+            x += GAP
+            ctxBoard.moveTo(GAP, y)
+            ctxBoard.lineTo(GAP + GAP * COLS, y)
+            y += GAP
+        }
+        ctxBoard.stroke()
+    })
 
-    // draw board lines
-    let x: number = GAP
-    let y: number = GAP 
-    while (x < width || y < height) {
-        ctxBoard.moveTo(x, GAP)
-        ctxBoard.lineTo(x, GAP + GAP * ROWS)
-        x += GAP
-        ctxBoard.moveTo(GAP, y)
-        ctxBoard.lineTo(GAP + GAP * COLS, y)
-        y += GAP
+    // show transparent shadow of piece when hovering
+    function hovering(e: MouseEvent) {
+        // set closest coordinates for piece
+        let closestX: number = Math.round((e.clientX - boardX) / GAP) * GAP
+        let closestY: number = Math.round((e.clientY - boardY) / GAP) * GAP
+
+        // cap closestX and closestY
+        closestX = Math.min(GAP + GAP * COLS, Math.max(GAP, closestX))
+        closestY = Math.min(GAP + GAP * ROWS, Math.max(GAP, closestY))
+
+        // draw shadow
+        ctxHover.clearRect(0, 0, width, height)
+        ctxHover.beginPath()
+        ctxHover.arc(closestX, closestY, GAP / 2, 0, 2 * Math.PI)
+        ctxHover.fillStyle = pieceColor
+        ctxHover.fill()
     }
-    ctxBoard.stroke()
-})
 
-// show transparent shadow of piece when hovering
-function hovering(e: MouseEvent) {
-    // set closest coordinates for piece
-    let closestX: number = Math.round((e.clientX - boardX) / GAP) * GAP
-    let closestY: number = Math.round((e.clientY - boardY) / GAP) * GAP
+    // place piece on board when clicked
+    async function placing(e: MouseEvent) {
+        // set closest coordinates for piece
+        let closestX: number = Math.round((e.clientX - boardX) / GAP) * GAP
+        let closestY: number = Math.round((e.clientY - boardY) / GAP) * GAP
 
-    // cap closestX and closestY
-    closestX = Math.min(GAP + GAP * COLS, Math.max(GAP, closestX))
-    closestY = Math.min(GAP + GAP * ROWS, Math.max(GAP, closestY))
+        // cap closestX and closestY
+        closestX = Math.min(GAP + GAP * COLS, Math.max(GAP, closestX))
+        closestY = Math.min(GAP + GAP * ROWS, Math.max(GAP, closestY))
 
-    // draw shadow
-    ctxHover.clearRect(0, 0, width, height)
-    ctxHover.beginPath()
-    ctxHover.arc(closestX, closestY, GAP / 2, 0, 2 * Math.PI)
-    ctxHover.fillStyle = pieceColor
-    ctxHover.fill()
-}
+        // check if move is legal
+        let x: number = Math.floor((closestY - GAP) / GAP)
+        let y: number = Math.floor((closestX - GAP) / GAP)
+        let color: number = pieceColor === 'black' ? 1 : 2
+        let isValid: boolean = await invoke('validate', { x, y, color })
+        if (!isValid) return
 
-// place piece on board when clicked
-function placing(e: MouseEvent) {
-    // set closest coordinates for piece
-    let closestX: number = Math.round((e.clientX - boardX) / GAP) * GAP
-    let closestY: number = Math.round((e.clientY - boardY) / GAP) * GAP
+        // draw piece
+        ctxPieces.beginPath()
+        ctxPieces.arc(closestX, closestY, GAP / 2, 0, 2 * Math.PI)
+        ctxPieces.fillStyle = pieceColor
+        ctxPieces.fill()
 
-    // cap closestX and closestY
-    closestX = Math.min(GAP + GAP * COLS, Math.max(GAP, closestX))
-    closestY = Math.min(GAP + GAP * ROWS, Math.max(GAP, closestY))
-
-    // check if move is legal
-    let row: number = Math.floor((closestY - GAP) / GAP)
-    let col: number = Math.floor((closestX - GAP) / GAP)
-    if (boardArray[row][col] !== 0) return
-
-    // draw piece
-    ctxPieces.beginPath()
-    ctxPieces.arc(closestX, closestY, GAP / 2, 0, 2 * Math.PI)
-    ctxPieces.fillStyle = pieceColor
-    ctxPieces.fill()
-
-    // set piece color and array
-    pieceColor = pieceColor === 'black' ? 'white' : 'black'
-    boardArray[row][col] = pieceColor === 'black' ? 1 : 2
-}
-
+        // set piece color
+        pieceColor = pieceColor === 'black' ? 'white' : 'black'
+    }
 </script>
 
 <canvas bind:this={board} class="absolute top-0 left-0" {width} {height}></canvas>
